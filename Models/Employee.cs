94db0bbf2f653dde12cf91a30dfc3bf9afc86387
vasileios-kortsinoglou <v.kortsinoglou@yahoo.com>
2025-02-08 18:46:17 +0200

    Initial commit

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication6.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Age { get; set; }
+
+        public int IsActive { get; set; }
+
+
+    }
+}
---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication6.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options): base(options)
+
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
